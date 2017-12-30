97dd6e3630ae6a779cc8dbac681ef3e6a751e556
mahacoder <ananthan31@gmail.com>
2017-12-29 16:42:27 -0800

    Added solution using stack O(n) time

--- Python/q496_next_greater_element_1.py
+++ Python/q496_next_greater_element_1.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution:
     def nextGreaterElement(self, nums1, nums2):
         """
@@ -6,6 +8,30 @@ class Solution:
         :rtype: List[int]
         """
 
+        # return brute_force(nums1, nums2)
+        return self.stack_solution(nums1, nums2)
+
+    def stack_solution(self, nums1, nums2):
+        ans = [-1]*len(nums1)
+        map = defaultdict()
+        for i, v in enumerate(nums1):
+            map[v]=i
+
+        stack = []
+        for i, v in enumerate(nums2):
+            if len(stack)==0:
+                stack.append(v)
+            else:
+                if v>stack[-1]:
+                    while len(stack)!=0 and stack[-1]<v:
+                        elem = stack.pop(-1)
+                        if elem in map:
+                            ans[map[elem]] = v
+                    stack.append(v)
+                else: stack.append(v)
+        return ans
+
+    def brute_force(self, nums1, nums2):
         ans = [-1]*len(nums1)
         for i in range(len(nums1)):
             found = False
