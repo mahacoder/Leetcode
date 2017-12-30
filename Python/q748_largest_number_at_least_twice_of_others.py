00bca3a507089b116c6329b5f77effbf96f5a188
mahacoder <ananthan31@gmail.com>
2017-12-29 21:10:21 -0800

    One pass solution

--- Python/q748_largest_number_at_least_twice_of_others.py
+++ Python/q748_largest_number_at_least_twice_of_others.py
@@ -4,7 +4,24 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        return self.one_pass(nums)
 
+    def one_pass(self, n):
+        maxm, smaxm = -1, -1
+        idxm = -1
+        for i in range(len(n)):
+            if n[i]>smaxm and n[i]<maxm:
+                smaxm = n[i]
+            elif n[i]>maxm:
+                smaxm = maxm
+                maxm=n[i]
+                idxm = i
+        if maxm==smaxm: return idxm
+        else:
+            if 2*smaxm<=maxm: return idxm
+            else: return -1
+
+    def two_pass(self, nums):
         maxm = max(nums)
         flag, idx = True, -1
         for i in range(len(nums)):
