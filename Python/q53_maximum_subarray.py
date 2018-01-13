19d844ed1514142e9ba586ad962c95721a2d5a79
mahacoder <ananthan31@gmail.com>
2018-01-13 14:22:47 -0800

    Added memoized solution

--- Python/q53_maximum_subarray.py
+++ Python/q53_maximum_subarray.py
@@ -4,7 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # return self.tabulation(nums)
+        max_till_index = {}
+        for i in range(len(nums)):
+            self.memoized(nums, i, max_till_index)
+        max = max_till_index[0]
+        for v in max_till_index.values():
+            if v>max: max=v
+        return max
 
+    def memoized(self, nums, index, max_till_index):
+        if index in max_till_index: return max_till_index[index]
+
+        if index==0:
+            max_till_index[index]=nums[index]
+            return max_till_index[index]
+
+        max_till_index[index]=max(nums[index]+self.memoized(nums,index-1, max_till_index), nums[index])
+        return max_till_index[index]
+
+
+    def tabulation(self, nums):
         max_sum = [0]*len(nums)
         max_sum[0] = nums[0]
         for i in range(1, len(nums)):
